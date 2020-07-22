9959aeef72e172c1199847f36c9ac10f5fb3acef
Alexei Yagolnikov <alexei.yagolnikov@skyward.io>
2020-07-21 21:43:16 -0700

    Improved performance and add robot players

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,9 @@ const { v4: uuid } = require('uuid');
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-// var CWD = process.cwd();
-const usersById = {};
 let batchTimer = null;
 let batchedMessagesById = {};
-// const userIsConnectedTimersById = {};
+let robotsById = {};
 let lastMsgTime = 0;
 let isIdle = false;
 let fps = 30;
@@ -16,14 +14,13 @@ http.listen(3001, function(){
 });
 
 io.on('connection', function(socket) {
-  console.log('newPlayerUuid', socket.id, socket.connected);
-  usersById[socket.id] = true;
+  console.log('New Player ID:', socket.id);
   socket.emit('connected', socket.id);
 
   socket.on('disconnect', function (msg) {
-    console.log('disconnected', socket.id, msg);
+    console.log('Player ID disconnected', socket.id, msg);
     io.emit('user disconnected', socket.id);
-    delete usersById[socket.id];
+    delete batchedMessagesById[socket.id];
   });
 
   socket.on('message', (msg) => {
@@ -32,11 +29,40 @@ io.on('connection', function(socket) {
     Object.keys(msg).map((key) => {
       batchedMessagesById[key] = msg[key];
     });
-    if (!batchTimer) initTimer();
   });
 
   initTimer();
 });
+
+function generateAIPlayers() {
+  let count = 1;
+  do {
+    const newPlayerUuid = uuid();
+    robotsById[newPlayerUuid] = {
+      id: newPlayerUuid,
+      isIdle: true,
+      isRobot: true,
+      pos: {
+        x: -122.67399123828811,
+        y: 45.53044800949424
+      },
+      velocity: {
+        x: 0,
+        y: 0,
+      },
+    }
+    count -= 1;
+  } while (count > 0)
+}
+
+// function
+
+function animateRobotPlayers() {
+  Object.values(robotsById).map((robot) => {
+    robot.pos.x += (Math.random() - 0.5) * 0.0001;
+    robot.pos.y += (Math.random() - 0.5) * 0.0001;
+  });
+}
 
 function removeExpiredObjects(byId) {
   return Object.values(byId).reduce((sum, item) => {
@@ -48,16 +74,22 @@ function removeExpiredObjects(byId) {
 }
 
 function initTimer() {
+  if (batchTimer) return;
   batchTimer = setInterval(() => {
+    animateRobotPlayers();
     batchedMessagesById = removeExpiredObjects(batchedMessagesById);
 
     if (lastMsgTime - Date.now() > 1000) {
       isIdle = true;
       clearInterval(batchTimer);
     }
-    if (Object.keys(batchedMessagesById).length) {
-      // console.log(batchedMessagesById);
-      io.emit('message-all', batchedMessagesById);
+    if (Object.keys(batchedMessagesById).length || Object.keys(robotsById).length) {
+      io.emit('message-all', {
+        ...batchedMessagesById,
+        ...robotsById,
+      });
     }
   }, 1000 / fps);
+
+  generateAIPlayers();
 }
